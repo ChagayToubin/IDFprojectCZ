070172f1fad093c79ed69a8a37ccf1faa20b9b61
chagay <chagaytoubin@gmail.com>
2025-05-21 18:20:16 +0300

    first commit

---
+++ IntelligenceDepartment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idf
+{
+    internal class IntelligenceDepartment
+    {
+        public Hamas hamas { get; set; }
+        public IntelligenceDepartment(Hamas Hamass)
+        {
+            hamas = Hamass;
+            AddMessage();
+        }
+        public void AddMessage()
+        {
+            Random rnd = new Random();
+
+            List<string> message = new List<string>
+            {
+                "Buildings",
+                "people",
+                "armored vehicles",
+                "open areas"
+
+            };
+            Console.WriteLine("");
+            foreach (var item in hamas.ListTerrorist)
+            {
+                item.TimeLocatiom[DateTime.UtcNow] = message[rnd.Next(0, 4)];
+
+            }
+            Console.WriteLine("1234567890");
+
+        }
+
+    }
+}
---
+++ Terrorist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idf
+{
+    internal class Terrorist
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        public bool Status { get; set; }
+
+        public List<string> Weapon { get; set; } = new List<string>();
+
+        public Dictionary<DateTime, string> TimeLocatiom { get; set; } = new Dictionary<DateTime, string>();//אמרנו שמפתח יהיה זמן כך יהיה ושנה מהכל ובערך יהיה הודעה עם מיקום
+        public Terrorist(string name, int rank, bool status, List<string> weapon)
+        {
+            Name = name;
+            Rank = rank;
+            Status = status;
+            Weapon = new(weapon);
+
+        }
+
+    }
+
+}
