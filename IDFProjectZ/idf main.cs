55de27eceda47658eb587bccd3185c177541306c
tzirkus <zalmen5779@gmail.com>
2025-05-22 11:30:12 +0300

    Initial commit

---
+++ IDFProjectZ/idf main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using IDFProject.attack_units;
+using IDFProject;
+namespace IDFProject.idf;
+
+public class IdfMain
+{
+    public string DateOfEstablishment { get; set; }
+    public string CurrentCensus { get; set; }
+    public List<AttackUnits> AtackUnits { get; set; } = new List<AttackUnits>();
+    public IdfMain(string date_of_establishment, string current_census)
+    {
+        DateOfEstablishment = date_of_establishment;
+        CurrentCensus = current_census;
+    }
+    public void AddAttackUnit(AttackUnits attack_unit)
+    {
+        AtackUnits.Add(attack_unit);
+    }
+}
