00ded26c9fedc3536b5fb1a0006f10e9ffd09a20
chagay <chagaytoubin@gmail.com>
2025-05-23 17:26:56 +0300

    שבת שלום1

--- obj/Debug/net8.0/IDFprojectC.AssemblyInfo.cs
+++ obj/Debug/net8.0/IDFprojectC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDFprojectC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1da94141c9a0b6ebf1e5eac1cdc158412ae4b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b160ec373158a24b441b94847cf7cc003f79fc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDFprojectC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDFprojectC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
